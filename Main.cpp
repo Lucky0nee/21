23b3d8d5140d5e4ce44704807ed0f2a941b9987f
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-01-17 22:17:05 +0200

    Update and rename README.md to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	system("chcp 1251>nul");
+	string String;
+	string Temp;
+	while (String.length() == 0)
+		getline(cin, String);
+
+	Temp += String;
+	reverse(String.begin(), String.end());
+
+	if (String == Temp)
+		cout << Temp << " - Палiндромом";
+	else
+		cout << Temp << " - не є Палiндромом";
+	// козак з казок
+}
+
+/*
+Є рядок символів. Потрібно перевірити, чи є він паліндромом. Тобто чи читається однаково в обох
+напрямках. Наприклад: “козак з казок”.
+*/
